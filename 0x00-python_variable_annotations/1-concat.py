42679bbf0fb065dabba4a14ce994c0c0dc420e5d
Natihdjdjdjdjdjdjdjdjdjd <natasbest@gmail.com>
2024-05-02 00:50:29 -0700

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""the concat  function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """lets take str1 and str2 variables and returns their concatenation"""
+    return str1 + str2
