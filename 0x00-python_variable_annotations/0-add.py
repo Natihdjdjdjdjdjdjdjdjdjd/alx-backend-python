f47648cadd75b4dfc21daae7538e5e3a5deb60a5
Natihdjdjdjdjdjdjdjdjdjd <natasbest@gmail.com>
2024-05-02 00:45:38 -0700

    add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""module that sum two number function add"""
+
+
+def add(a: float, b: float) -> float:
+    """Takes two floats float a and b then returns their sum as float"""
+    return a + b
